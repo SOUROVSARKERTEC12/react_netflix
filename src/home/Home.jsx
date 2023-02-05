0501f4bc4746e745bcbb9d099536184e664eb3ec
SOUROV <sourovsarker005@gmail.com>
2023-02-06 01:36:41 +0600

    Simple Navbar

---
+++ src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from "../components/navbar/Navbar";
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Navbar/>
+            <img width="100%"
+                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                 alt=""/>
+        </div>
+    );
+};
+
+export default Home;
