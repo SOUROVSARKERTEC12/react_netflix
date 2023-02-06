60fe07c5d00c2d80562cd4665cd15387b5d8ae4b
SOUROV <sourovsarker005@gmail.com>
2023-02-07 00:32:29 +0600

    Netflix Featured Movie

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navbar from "../components/navbar/Navbar";
 import './home.scss'
+import Featured from "../components/featured/Featured";
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar/>
-            <img width="100%"
-                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                 alt=""/>
+            {/*<img width="100%"*/}
+            {/*    src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"*/}
+            {/*     alt=""/>*/}
+            <Featured/>
         </div>
     );
 };
