4ed41bd1754b3f22a2c7a2de20e2cda1b98d46a3
SOUROV <sourovsarker005@gmail.com>
2023-02-07 01:24:33 +0600

    Netflix Slider

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from "../components/navbar/Navbar";
 import './home.scss'
 import Featured from "../components/featured/Featured";
+import List from "../components/list/List";
+
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar/>
-            {/*<img width="100%"*/}
-            {/*    src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"*/}
-            {/*     alt=""/>*/}
             <Featured/>
+            <List/>
+            <List/>
+            <List/>
+            <List/>
         </div>
     );
 };
